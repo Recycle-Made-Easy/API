11e68abab943858ce783170cfbf3fbc7e14da5b7
unknown <alexgochenour@gmail.com>
2019-11-20 14:11:25 -0500

    Added some hash and toString() and various other things the endpoints to get all recyclables and categories still do not work.

--- src/main/java/api/api/entities/Recyclable.java
+++ src/main/java/api/api/entities/Recyclable.java
@@ -19,6 +19,8 @@ public class Recyclable {
 		this.name = name;
 	}
 
+	protected Recyclable () {}
+	
 	public Long getId() {
 		return id;
 	}
@@ -37,6 +39,42 @@ public class Recyclable {
 	
 	public void updateCategory(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Recyclable [id=" + id + ", name=" + name + ", category=" + category + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recyclable other = (Recyclable) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 	
 
--- src/main/java/controllers/RecyclableController.java
+++ src/main/java/controllers/RecyclableController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +20,11 @@ public class RecyclableController {
 	private RecyclableService recyclableService;
 	
 	@GetMapping("/")
-	public Iterable<Recyclable> getAllRecyclables(){
-		return recyclableService.findAllRecyclables();
+	public List<Recyclable> getAllRecyclables(){
+		return (List<Recyclable>) recyclableService.findAllRecyclables();
 	}
 	
-	@GetMapping("{id}")
+	@GetMapping("/{id}")
 	public Recyclable getSingleRecyclable(@PathVariable Long id) {
 		return recyclableService.findRecyclableById(id);
 	}
