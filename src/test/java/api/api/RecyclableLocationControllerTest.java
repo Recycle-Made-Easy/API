6a5e5c0da198ba60d777edfc90fd9f5753386719
robby <robby.remmenga@gmail.com>
2019-11-21 10:38:24 -0500

    passing test with recyclablecontroller

---
+++ src/test/java/api/api/RecyclableLocationControllerTest.java
@@ -0,0 +1,56 @@
+package api.api;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.core.Is.is;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import api.api.controllers.RecyclabelLocationController;
+import api.api.entities.RecycleLocation;
+import api.api.services.RecycleLocationService;
+
+public class RecyclableLocationControllerTest {
+	
+	@InjectMocks
+	RecyclabelLocationController underTest;
+	
+	@Mock
+	RecycleLocationService recycleLocationService;
+	@Mock
+	private RecycleLocation mockRecycleLocation;
+	
+	private MockMvc mockMvc;
+	private RecycleLocation testRecycleLocation;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		testRecycleLocation = new RecycleLocation("Dublin");
+		mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
+	}
+	
+	@Test
+	public void findAllReturnsListOfRecyclableLocations() {
+		when(recycleLocationService.findAllRecycleLocations()).thenReturn(Collections.singletonList(testRecycleLocation));
+		List<RecycleLocation> retrievedRecyclableLocation = underTest.findAll();
+		assertThat(retrievedRecyclableLocation, contains(testRecycleLocation));
+	}
+	
+	@Test
+	public void findByNameReturnsSingleRecyclableLocation() {
+		when(recycleLocationService.findRecycleLocationByName("Dublin")).thenReturn(testRecycleLocation);
+		RecycleLocation retrievedRecyclableLocation = underTest.findRecyclableLocationByName("Dublin");
+		assertThat(retrievedRecyclableLocation, is(testRecycleLocation));
+	}
+
+}
